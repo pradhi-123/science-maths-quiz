c53cdb26b92534da4431dc37f32470018c148987
Pradhiksha <pradhikshaanandkumar@gmail.com>
2026-07-16 14:59:18 +0530

    fix: Drastically lower BGM volume to 8 percent

--- src/pages/PresenterPage.jsx
+++ src/pages/PresenterPage.jsx
@@ -594,7 +594,7 @@ export default function PresenterPage({ navigateTo }) {
           src={activeBGM}
           autoPlay 
           loop 
-          onCanPlay={(e) => { e.target.volume = 0.2; }}
+          onCanPlay={(e) => { e.target.volume = 0.08; }}
           style={{ display: 'none' }}
         />
       )}
